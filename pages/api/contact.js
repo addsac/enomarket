b368da9174cae00f918ee3e7752b421852401e07
Leonardo Citton <>
2023-02-28 11:16:31 +0100

    setting up sendgrid api route

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,8 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mail from '@sendgrid/mail'
 
-export default function handler(req, res) {
+mail.setApiKey(process.env.SENDGRID_API_KEY)
+
+export default async function handler(req, res) {
     const { fullname, company_name, email, message} = req.body
 
-    res.status(200).json({ data: 'Email inviata a ' + email + '.' })
+    const data = {
+      to: 'info@enomarket.eu',
+      from: 'pegasodigitalstudio@gmail.com',
+      subject: `New message from ${fullname} (company: ${company_name}) - ${email}`,
+      text: message,
+      html: fullText.replace(/\r\n/g, '<br />'),
+    }
+
+    await mail.send(data)
+        .catch((err) => console.log(err.response.body))
+
+    res.status(200).json({ status: 'OK' })
   }
   
