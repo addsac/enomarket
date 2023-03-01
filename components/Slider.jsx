36cb25a95b1cd083dcbb95cf1f6d6feedaab736f
Leonardo Citton <>
2023-03-01 15:40:41 +0100

    refactoring for online test

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -28,25 +28,27 @@ export default function Slider({ sliderData }) {
           className="mySwiper"
         >
           {sliderData.map( item => (
-            <SwiperSlide className='relative bg-gray-900'>
-              {/* gradient left */}
-              <div className="absolute z-[-1] top-0 bottom-0 w-[500px] bg-gradient-to-r from-gray-900/80 to-transparent"></div>
+            <div key={item.sys.id}>
+              <SwiperSlide className='relative bg-gray-900'>
+                {/* gradient left */}
+                <div className="absolute z-[-1] top-0 bottom-0 w-[500px] bg-gradient-to-r from-gray-900/80 to-transparent"></div>
 
-              <img src={item.fields.foto.fields.file.url} alt="" className="z-[-2] absolute w-full h-full object-cover opacity-40" />
-  
-              <div key={item.sys.id} className="w-full h-full grid grid-cols-12 gap-x-[17px] md:gap-x-6 px-6 md:px-16 pb-[40px] lg:pb-[100px]">
-                <div className="col-span-12 lg:col-span-8 h-full flex flex-col justify-end gap-y-10">
-                  <p className='uppercase text-16 lg:text-18 font-familiy-monserrat font-medium text-yellow-400'> { item.fields.categoria } </p>
-                  <p className='text-44 lg:text-58 xl:text-78 font-family-lora text-white leading-[130%]'> { item.fields.titolo } </p>
-                  <div>
-                    <ButtonSecondary
-                      text={ item.fields.bottone }
-                      href={ item.fields.link }
-                    />
+                <img src={item.fields.foto.fields.file.url} alt="" className="z-[-2] absolute w-full h-full object-cover opacity-40" />
+    
+                <div className="w-full h-full grid grid-cols-12 gap-x-[17px] md:gap-x-6 px-6 md:px-16 pb-[40px] lg:pb-[100px]">
+                  <div className="col-span-12 lg:col-span-8 h-full flex flex-col justify-end gap-y-10">
+                    <p className='uppercase text-16 lg:text-18 font-familiy-monserrat font-medium text-yellow-400'> { item.fields.categoria } </p>
+                    <p className='text-44 lg:text-58 xl:text-78 font-family-lora text-white leading-[130%]'> { item.fields.titolo } </p>
+                    <div>
+                      <ButtonSecondary
+                        text={ item.fields.bottone }
+                        href={ item.fields.link }
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
+            </div>
           ))}
         </Swiper>
     </div>
