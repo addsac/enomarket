eded0a5c00dc39e1e5519b90e94e6ad7bc5b316d
Leonardo Citton <>
2023-03-16 18:27:34 +0100

    refactoring contact api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -19,12 +19,8 @@ export default async function handler(req, res) {
     html: fullText.replace(/\r\n/g, '<br />'),
   }
 
-  try {
-    await sendgrid.send(data);
-  } catch (error) {
-    return res.status(error.statusCode || 500).json({ error: error.message });
-  }
+  await mail.send(data)
+    .catch((err) => console.log(err.response.body))
 
   res.status(200).json({ status: 'OK' })
 }
-  
