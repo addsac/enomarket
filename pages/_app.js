54ce6d485e88d5464915bf76099a1f00b2695dcd
Leonardo Citton <>
2023-03-30 18:16:31 +0200

    page transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '@/styles/globals.css'
+import { motion, AnimatePresence } from 'framer-motion'
 import { Montserrat, Lora, Open_Sans  } from '@next/font/google'
+import { useRouter } from 'next/router'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 const lora = Lora({ subsets: ['latin'] })
 const openSans = Open_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter().asPath
+
   return <>
     <style jsx global>{`
       html {
@@ -18,6 +22,20 @@ export default function App({ Component, pageProps }) {
         font-family: ${lora.style.fontFamily};
       }
     `}</style>
-    <Component {...pageProps}  />
+      <AnimatePresence
+        mode='wait'
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <motion.div
+          key={router}
+          initial={{ opacity: 0, y: 17 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 17 }}
+          transition={{ duration: 0.5, ease: 'easeInOut' }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
   </>
 }
