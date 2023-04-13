0d58d984d72c1fb1720019dc30eecc8a9d06f71a
Leonardo Citton <>
2023-04-13 09:23:00 +0200

    edited slider texts with Balancer.

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -4,6 +4,7 @@ import { Autoplay, Pagination } from 'swiper'
 import Image from 'next/image'
 import 'swiper/css'
 import 'swiper/css/pagination'
+import { Balancer } from 'react-wrap-balancer'
 
 export default function Slider({ sliderData }) {
   return (
@@ -45,7 +46,11 @@ export default function Slider({ sliderData }) {
                 <div className="w-full h-full grid grid-cols-12 gap-x-[17px] md:gap-x-6 px-6 md:px-16 pb-[40px] lg:pb-[100px]">
                   <div className="col-span-12 lg:col-span-8 h-full flex flex-col justify-end gap-y-10">
                     <p className='uppercase text-16 lg:text-18 font-familiy-monserrat font-medium text-yellow-400 -mb-2'> { item.fields.categoria } </p>
-                    <p className='text-[32px] lg:text-58 font-family-lora text-white leading-[130%]'> { item.fields.titolo } </p>
+                    <p className='text-[32px] lg:text-58 font-family-lora text-white leading-[130%]'> 
+                      <Balancer>
+                        { item.fields.titolo }
+                      </Balancer>
+                    </p>
                     <div className='mt-2'>
                       <ButtonSecondary
                         text={ item.fields.bottone }
