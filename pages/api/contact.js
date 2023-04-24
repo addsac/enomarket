0538fe2ed3c30ef17f5902170c621f25bc9719ed
Leonardo Citton <>
2023-04-24 10:13:29 +0200

    debug email

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -22,9 +22,11 @@ export default async function handler(req, res) {
   await mail.send(data)
   .then((res) => {
     console.log('Email sent:', res)
+    console.log(res.errors.body)
   })
   .catch((error) => {
-    console.error(error)
+    console.log(error)
+    console.log(error.errors.body)
   })
 
   res.status(200).json({ status: 'OK' })
